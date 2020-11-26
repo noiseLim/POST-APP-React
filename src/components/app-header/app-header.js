dba4dcba1f9153e132f4929bc1622a1a8fddf108
noiseLim <noiselim@gmail.com>
2020-11-26 14:50:53 +0300

    first APP to React

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div>
+            <h1>Aleksey Fedotov</h1>
+            <h2>5 записей, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './post-list-item.css';
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className="app-list-item-label">
+                Hello world!
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button
+                    type="button"
+                    className="btn-star btn-sm">
+                        <i className="fa fa-star"></i>
+                </button>
+                <button
+                    type="button"
+                    className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                </button>
+                <i className="fa fa-heart"></i>
+            </div>
+        </li>
+    )
+}
+
+export default PostListItem;
