2f80c187af05514a4e4a8d89c98edb857ef19365
noiseLim <noiselim@gmail.com>
2020-11-28 22:46:34 +0300

    bootstrap

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PostListItem from '../post-list-item';
+import { ListGroup } from 'reactstrap';
 
 import './post-list.css';
 
@@ -15,18 +16,28 @@ const PostList = ({posts}) => {
     //     )
     // });
     const elements = posts.map((item) => {
-        const {id, ...itemProps} = item
-        return (
-            <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} />
-            </li>
-        )
+
+        if (typeof item === 'object' && isEmpty(item)) {
+            const {id, ...itemProps} = item
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem {...itemProps} />
+                </li>
+            )
+        }
     });
 
+    function isEmpty(obj) {
+        for (let key in obj) {
+            return true;
+        }
+        return false;
+    }
+
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Button} from 'reactstrap';
+// import { Button } from 'react-bootstrap'
 
 import './post-status-filter.css';
 
 const PostStatusFilter = () => {
     return (
         <div className="btn-group">
+            {/* <Button color="info">Все</Button> */}
+            <Button variant="primary">Primary</Button>
             <button type='button' className='btn btn-info'>Все</button>
             <button type='button' className='btn btn-outline-secondary'>Понравилось</button>
             
