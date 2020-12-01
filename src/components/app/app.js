f458eb41990d4160cf039a22e7200e80fe68cfaf
noiseLim <noiselim@gmail.com>
2020-12-01 12:01:11 +0300

    finish script 'delete post'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,25 +14,47 @@ const AppBlock = styled.div`
       margin: 0 auto;
       max-width: 800px;
 `
-// const StyledAppBlock = styled(AppBlock)`
-//       background-color: grey;
-// `
 
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data : [
-                {label: "Going to learn React", important: true, id:"dfsd"},
-                {label: "That is so good", important: false, id:"dfsasd"},
-                {label: "I need a break", important: false, id:"dfd"},
+                {label: "Going to learn React", important: true, id: 1},
+                {label: "That is so good", important: false, id: 2},
+                {label: "I need a break", important: false, id: 3},
             ]
         };
         this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+
+        this.maxId = 4;
     }
 
     deleteItem(id) {
-        console.log(id);
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    addItem(body) {
+        const newItem = {
+              label: body,
+              important: false,
+              id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        })
     }
 
     render() {
@@ -46,7 +68,8 @@ export default class App extends Component {
                 <PostList
                 posts={this.state.data}
                 onDelete={this.deleteItem}/>
-                <PostAddForm/>
+                <PostAddForm
+                onAdd={this.addItem}/>
             </AppBlock>          
         )
     }
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './post-add-form.css';
 
-const PostAddForm = () => {
+const PostAddForm = ({onAdd}) => {
     return (
-        <form className="bottom-panel d-flex">
+        <div className="bottom-panel d-flex">
             <input
                 type="text"
                 placeholder="О чем вы думаете?"
@@ -12,9 +12,10 @@ const PostAddForm = () => {
             />
             <button
                 type="submit"
-                className="btn btn-outline-secondary">
+                className="btn btn-outline-secondary"
+                onClick={() => onAdd('Hello')}>
                 Добавить</button>
-        </form>
+        </div>
     )
 }
 
