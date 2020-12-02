17683cec537e2aa4cb2559036b3a0359a79ba193
noiseLim <noiselim@gmail.com>
2020-12-02 16:31:56 +0300

    more forms

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -23,12 +23,14 @@ export default class App extends Component {
                 {label: "Going to learn React", important: true, like: false, id: 1},
                 {label: "That is so good", important: false, like: false, id: 2},
                 {label: "I need a break", important: false, like: false, id: 3},
-            ]
+            ],
+            term: ''
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
 
         this.maxId = 4;
     }
@@ -85,11 +87,27 @@ export default class App extends Component {
         })
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+
     render() {
 
-        const {data} = this.state;
+        const {data, term} = this.state;
+
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
+        
+        const visiblePost = this.searchPost(data, term);
         
         return (
             <AppBlock>
@@ -97,11 +115,12 @@ export default class App extends Component {
                 liked={liked}
                 allPosts={allPosts}/>
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}/>
                     <PostStatusFilter/>
                 </div>
                 <PostList
-                posts={this.state.data}
+                posts={visiblePost}
                 onDelete={this.deleteItem}
                 onToggleImportant={this.onToggleImportant}
                 onToggleLiked={this.onToggleLiked}/>
--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
-    return (
-        <div className="bottom-panel d-flex">
-            <input
-                type="text"
-                placeholder="О чем вы думаете?"
-                className="form-control new-post-label"
-            />
-            <button
-                type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => onAdd('Hello')}>
-                Добавить</button>
-        </div>
-    )
-}
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange=this.onValueChange.bind(this);
+        this.onSubmit=this.onSubmit.bind(this);
+    }
 
-export default PostAddForm;
+    onValueChange(e) {
+        this.setState ({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text)
+        this.setState ({
+            text: ''
+        })
+    }
+
+    render() {
+        return (
+            <form 
+            className="bottom-panel d-flex"
+            onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    placeholder="О чем вы думаете?"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <button
+                    type="submit"
+                    className="btn btn-outline-secondary">
+                    Добавить</button>
+            </form>
+        )
+    }
+}
